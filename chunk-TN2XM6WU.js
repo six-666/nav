bfb557447d71442e0699ae98319c7bab60d5b791
xjh22222228 <xjh22222228@gmail.com>
2025-06-05 08:19:34 +0000

    Deploying to gh-pages from @ xjh22222228/nav@ed09db2ce215bbe7f3ec8773b970129d467d724a 🚀

--- chunk-TN2XM6WU.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-MU76UY3X.js";import{Hb as e,xb as r}from"./chunk-WMAU45KI.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
